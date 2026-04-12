17680acc2bf75c2b40b1a2b9855159b41bd65df7
Basavaraj Patil <patil.ba@northeastern.edu>
2026-04-12 02:22:34 -0400

    Add: sum-of-two-integers - submission-10

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-10.py
@@ -0,0 +1,9 @@
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        a = a & 0xFFFFFFFF
+        b = b  & 0xFFFFFFFF
+        while b:
+            a, b = (a ^ b)  & 0xFFFFFFFF, (a & b) << 1 & 0xFFFFFFFF
+        return a if not a & (1 << 31) else a - 0x100000000
+
+        
